3e1ad426d23f4a0dd303df2c9897b66a0578a49a
Claire Lesh <cteterita@gmail.com>
2020-09-28 13:06:07 -0700

    added header and footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React from 'react';
 function App() {
   return (
     <main className='App'>
+      <header>
+        <h1>Take 5</h1>
+      </header>
       {/* content goes here */}
+      <footer>
+        (c) 2020 Claire Teter Lesh - About - Send Feedback
+      </footer>
     </main>
   );
 }
