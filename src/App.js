5ba6b04b88d1a01e40c7f65915790e8ac14d0735
Claire Lesh <cteterita@gmail.com>
2020-09-29 14:44:30 -0700

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import DayView from './DayView/DayView';
-import LandingView from './LandingView/LandingView'
+import LandingView from './LandingView/LandingView';
 
 function App() {
   return (
@@ -11,8 +11,11 @@ function App() {
         <h1>Take 5</h1>
       </header>
       { /* Routes Here */ }
-      <Route exact path="/" component={LandingView} />
-      <Route exact path="/:date" component={DayView} />
+      <Switch>
+        <Route path="/today" component={DayView} />
+        <Route path="/:date(\d{4}-\d{2}-\d{2})" component={DayView} />
+        <Route path="/" component={LandingView} />
+      </Switch>
       <footer>
         (c) 2020 Claire Teter Lesh - About - Send Feedback
       </footer>
