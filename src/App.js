2ca5073386425dab8818f14fe5841945dcb7d5de
Claire Lesh <cteterita@gmail.com>
2020-09-29 12:55:09 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import DayView from './DayView/DayView';
 
 function App() {
   return (
-    <main className='App'>
+    <main className="App">
       <header>
         <h1>Take 5</h1>
       </header>
-      { /*Routes Here */ }
-      <DayView />
+      { /* Routes Here */ }
+      <Route exact path="/today" component={DayView} />
       <footer>
         (c) 2020 Claire Teter Lesh - About - Send Feedback
       </footer>
