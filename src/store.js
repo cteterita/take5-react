d69fb425cb9ed5b9639fefed6cd4b8d76dfe6bca
Claire Lesh <cteterita@gmail.com>
2020-09-29 15:37:44 -0700

    fixed linter errors in store

--- src/store.js
+++ src/store.js
@@ -4,25 +4,25 @@ const STORE = {
       complete: true,
       prompts: [
         {
-          prompt: `3 things I'm grateful for...`,
+          prompt: '3 things I\'m grateful for...',
           promptId: 1,
           responses: [
             'cats',
             'warm weather',
-            'React.js'
+            'React.js',
           ],
         },
         {
-          prompt: `3 things that will make today great...`,
+          prompt: '3 things that will make today great...',
           promptId: 2,
           responses: [
             'take5',
             'talking to Mom and Dad on the phone',
-            'a good workout'
+            'a good workout',
           ],
         },
         {
-          prompt: `My intention for today is...`,
+          prompt: 'My intention for today is...',
           promptId: 3,
           responses: [
             'focus',
@@ -34,7 +34,7 @@ const STORE = {
       complete: false,
       prompts: [
         {
-          prompt: `3 great things that happened today were...`,
+          prompt: '3 great things that happened today were...',
           promptId: 4,
           responses: [
             null,
@@ -43,46 +43,46 @@ const STORE = {
           ],
         },
         {
-          prompt: `One thing I let go of today was...`,
+          prompt: 'One thing I let go of today was...',
           promptId: 5,
           responses: [
             null,
           ],
         },
         {
-          prompt: `I am excited about tomorrow because...`,
+          prompt: 'I am excited about tomorrow because...',
           promptId: 6,
           responses: [
             null,
           ],
         },
       ],
-    }
+    },
   },
   '2020-09-26': {
     morning: {
       complete: true,
       prompts: [
         {
-          prompt: `3 things I'm grateful for...`,
+          prompt: '3 things I\'m grateful for...',
           promptId: 1,
           responses: [
             'cats',
             'warm weather',
-            'React.js'
+            'React.js',
           ],
         },
         {
-          prompt: `3 things that will make today great...`,
+          prompt: '3 things that will make today great...',
           promptId: 2,
           responses: [
             'take5',
             'talking to Mom and Dad on the phone',
-            'a good workout'
+            'a good workout',
           ],
         },
         {
-          prompt: `My intention for today is...`,
+          prompt: 'My intention for today is...',
           promptId: 3,
           responses: [
             'focus',
@@ -94,7 +94,7 @@ const STORE = {
       complete: true,
       prompts: [
         {
-          prompt: `3 great things that happened today were...`,
+          prompt: '3 great things that happened today were...',
           promptId: 4,
           responses: [
             'I met a talking dog',
@@ -103,46 +103,46 @@ const STORE = {
           ],
         },
         {
-          prompt: `One thing I let go of today was...`,
+          prompt: 'One thing I let go of today was...',
           promptId: 5,
           responses: [
             'Obsessively tracking my Amazon packages',
           ],
         },
         {
-          prompt: `I am excited about tomorrow because...`,
+          prompt: 'I am excited about tomorrow because...',
           promptId: 6,
           responses: [
             'I get to see that talking dog again',
           ],
         },
       ],
-    }
+    },
   },
   blank: {
     morning: {
       complete: false,
       prompts: [
         {
-          prompt: `3 things I'm grateful for...`,
+          prompt: '3 things I\'m grateful for...',
           promptId: 1,
           responses: [
             null,
             null,
-            null
+            null,
           ],
         },
         {
-          prompt: `3 things that will make today great...`,
+          prompt: '3 things that will make today great...',
           promptId: 2,
           responses: [
             null,
             null,
-            null
+            null,
           ],
         },
         {
-          prompt: `My intention for today is...`,
+          prompt: 'My intention for today is...',
           promptId: 3,
           responses: [
             null,
@@ -154,7 +154,7 @@ const STORE = {
       complete: false,
       prompts: [
         {
-          prompt: `3 great things that happened today were...`,
+          prompt: '3 great things that happened today were...',
           promptId: 4,
           responses: [
             null,
@@ -163,22 +163,22 @@ const STORE = {
           ],
         },
         {
-          prompt: `One thing I let go of today was...`,
+          prompt: 'One thing I let go of today was...',
           promptId: 5,
           responses: [
             null,
           ],
         },
         {
-          prompt: `I am excited about tomorrow because...`,
+          prompt: 'I am excited about tomorrow because...',
           promptId: 6,
           responses: [
             null,
           ],
         },
       ],
-    }
-  }
+    },
+  },
 };
 
 export default STORE;
