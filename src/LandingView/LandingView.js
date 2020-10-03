8d750718f6d1ffc304b836501c6460e041e87c3b
Claire Lesh <cteterita@gmail.com>
2020-10-03 16:38:19 -0700

    implemented sign in button

--- src/LandingView/LandingView.js
+++ src/LandingView/LandingView.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import AuthContext from '../AuthContext';
+
 function LandingView() {
+  const auth = useContext(AuthContext);
   return (
     <section id="daily-entry">
       <h3>Take 5 minutes each day for yourself</h3>
       <p>Set your intentions in the morning</p>
       <p>Practice gratitude in the evening</p>
+      <button type="submit" onClick={auth.signInWithGoogle}>
+        Sign in with Google
+      </button>
       <Link to="/today">
         <button type="submit">View Demo</button>
       </Link>
