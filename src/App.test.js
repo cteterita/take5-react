a3c95568d33c4682faf34cfe0c856a838f41b0f8
Claire Lesh <cteterita@gmail.com>
2020-10-03 14:36:07 -0700

    Added app component smoke test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/take 5/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('renders without errors', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
